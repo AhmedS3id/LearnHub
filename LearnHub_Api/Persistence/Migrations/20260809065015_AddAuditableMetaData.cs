92cf7bef5936661f4462d34eb497ac796cadf553
Ahmed Said <ahmeds3id711@gmail.com>
2026-08-09 09:52:24 +0300

    Add migrations and update dataBase for auditabletable

---
+++ LearnHub_Api/Persistence/Migrations/20260809065015_AddAuditableMetaData.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearnHub_Api.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditableMetaData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedById",
+                table: "Reviews",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedById",
+                table: "Reviews",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "Reviews",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedById",
+                table: "Lessons",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Lessons",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedById",
+                table: "Lessons",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "Lessons",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedById",
+                table: "Courses",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Courses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedById",
+                table: "Courses",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "Courses",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedById",
+                table: "Categories",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Categories",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedById",
+                table: "Categories",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_CreatedById",
+                table: "Reviews",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UpdatedById",
+                table: "Reviews",
+                column: "UpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_CreatedById",
+                table: "Lessons",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_UpdatedById",
+                table: "Lessons",
+                column: "UpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_CreatedById",
+                table: "Courses",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_UpdatedById",
+                table: "Courses",
+                column: "UpdatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CreatedById",
+                table: "Categories",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_UpdatedById",
+                table: "Categories",
+                column: "UpdatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_AspNetUsers_CreatedById",
+                table: "Categories",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_AspNetUsers_UpdatedById",
+                table: "Categories",
+                column: "UpdatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_AspNetUsers_CreatedById",
+                table: "Courses",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_AspNetUsers_UpdatedById",
+                table: "Courses",
+                column: "UpdatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_AspNetUsers_CreatedById",
+                table: "Lessons",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_AspNetUsers_UpdatedById",
+                table: "Lessons",
+                column: "UpdatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_CreatedById",
+                table: "Reviews",
+                column: "CreatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_UpdatedById",
+                table: "Reviews",
+                column: "UpdatedById",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_AspNetUsers_CreatedById",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_AspNetUsers_UpdatedById",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_AspNetUsers_CreatedById",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_AspNetUsers_UpdatedById",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_AspNetUsers_CreatedById",
+                table: "Lessons");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_AspNetUsers_UpdatedById",
+                table: "Lessons");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_CreatedById",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_UpdatedById",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_CreatedById",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_UpdatedById",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lessons_CreatedById",
+                table: "Lessons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lessons_UpdatedById",
+                table: "Lessons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_CreatedById",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_UpdatedById",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_CreatedById",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_UpdatedById",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedById",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedById",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedById",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedById",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Categories");
+        }
+    }
+}
