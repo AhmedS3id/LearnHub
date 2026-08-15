575e1635451160ecb61cc19440b6ddbd92a99ea6
Ahmed Said <ahmeds3id711@gmail.com>
2026-08-15 10:48:52 +0300

    update Enrollment Entitiess

--- LearnHub_Api/Entities/Enrollment.cs
+++ LearnHub_Api/Entities/Enrollment.cs
@@ -3,12 +3,11 @@
     public class Enrollment
     {
         public int Id { get; set; }
-        public string StudentId { get; set; }=string.Empty;
+        public string StudentId { get; set; } = string.Empty;
         public int CourseId { get; set; }
         public int Progress { get; set; }
-
-        public DateTime EnrolledOn = DateTime.UtcNow;
+        public DateTime EnrolledOn { get; set; } = DateTime.UtcNow;
         public Course Course { get; set; } = null!;
-        public ApplicationUser Student { get; set; }=null!;
+        public ApplicationUser Student { get; set; } = null!;
     }
 }
