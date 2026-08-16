22240a8822db63620ecf9957a8281ef76e376ce3
Ahmed Said <ahmeds3id711@gmail.com>
2026-08-16 09:38:16 +0300

    Refa : on Auth Services

--- LearnHub_Api/Services/AuthServices.cs
+++ LearnHub_Api/Services/AuthServices.cs
@@ -6,18 +6,21 @@ using LearnHub_Api.Errors;
 using LearnHub_Api.Helpers;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace LearnHub_Api.Services
 {
     public class AuthServices(UserManager<ApplicationUser> UserManager,
+        ApplicationDbContext context,
         ILogger<AuthServices>logger,
         IJwtProvider jwtProvider,
         IEmailSender emailSender,
         IHttpContextAccessor httpContextAccessor) : IAuthService
     {
         private readonly UserManager<ApplicationUser> _UserManager = UserManager;
+        private readonly ApplicationDbContext _context = context;
         private readonly ILogger<AuthServices> _logger = logger;
         private readonly IJwtProvider _jwtProvider = jwtProvider;
         private readonly IEmailSender _emailSender = emailSender;
@@ -44,7 +47,16 @@ namespace LearnHub_Api.Services
         }
         public async Task<Result<AuthResponse>> LoginAsync(LoginRequest request, CancellationToken cancellationToken)
         {
-            if (await _UserManager.FindByEmailAsync(request.Email) is not { } user)
+            //if (await _UserManager.FindByEmailAsync(request.Email) is not { } user)
+            //    return Result.Failure<AuthResponse>(UserErrors.InvalidCredentials);
+
+            var normalizedEmail = _UserManager.NormalizeEmail(request.Email);
+
+            var user = await _context.Users
+                .Include(x => x.RefreshTokens.Where(t => t.RevokedOn == null && t.ExpireOn > DateTime.UtcNow))
+                .FirstOrDefaultAsync(x => x.NormalizedEmail == normalizedEmail, cancellationToken);
+
+            if (user is null)
                 return Result.Failure<AuthResponse>(UserErrors.InvalidCredentials);
 
             if (!user.EmailConfirmed)
@@ -55,21 +67,26 @@ namespace LearnHub_Api.Services
 
             var (token, expireIn) = _jwtProvider.GenerateToken(user);
             var refreshToken = GenerateRefreshToken();
-            var RFExpirationDate = DateTime.UtcNow.AddDays(15);
+            var refreshTokenExpirationDate = DateTime.UtcNow.AddDays(15);
 
-            foreach (var tokens in user.RefreshTokens.Where(t => t.IsActive))
+            foreach (var activeToken in user.RefreshTokens.Where(x => x.IsActive))
             {
-                tokens.RevokedOn = DateTime.UtcNow;
+                activeToken.RevokedOn = DateTime.UtcNow;
             }
 
             user.RefreshTokens.Add(new RefreshToken
             {
                 Token = refreshToken,
-                ExpireOn = RFExpirationDate
+                ExpireOn = refreshTokenExpirationDate
             });
-            await _UserManager.UpdateAsync(user);
-            var response = new AuthResponse(user.Id, user.Email, user.FirstName, user.LastName, token, expireIn,refreshToken,RFExpirationDate);
-                return Result.Success(response);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            var response = new AuthResponse(
+                user.Id, user.Email, user.FirstName, user.LastName,
+                token, expireIn, refreshToken, refreshTokenExpirationDate);
+
+            return Result.Success(response);
         }
         public async Task<Result<AuthResponse>> GetRefreshTokenAsync(string Token, string RefreshToken, CancellationToken cancellationToken)
         {
@@ -77,13 +94,15 @@ namespace LearnHub_Api.Services
             if (userId is null)
                 return Result.Failure<AuthResponse>(UserErrors.InvalidJwtToken);
 
-            var user = await _UserManager.FindByIdAsync(userId);
+            var user = await _context.Users
+                .Include(x => x.RefreshTokens.Where(t => t.RevokedOn == null && t.ExpireOn > DateTime.UtcNow))
+                .FirstOrDefaultAsync(x => x.Id == userId, cancellationToken);
 
             if (user is null)
                 return Result.Failure<AuthResponse>(UserErrors.InvalidJwtToken);
 
             var userRefreshToken = user.RefreshTokens
-                .SingleOrDefault(x => x.Token == RefreshToken && x.IsActive);
+                .SingleOrDefault(x => x.Token == RefreshToken);
 
             if (userRefreshToken is null)
                 return Result.Failure<AuthResponse>(UserErrors.InvalidRefreshToken);
@@ -99,10 +118,11 @@ namespace LearnHub_Api.Services
                 Token = newRefreshToken,
                 ExpireOn = expirationDate
             });
-            await _UserManager.UpdateAsync(user);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
             var result = new AuthResponse(user.Id, user.Email, user.FirstName, user.LastName, newToken, expireIn, newRefreshToken, expirationDate);
             return Result.Success(result);
-
         }
 
         public async Task<Result> RevokeRefreshTokenAsync(string Token, string RefreshToken, CancellationToken cancellationToken)
@@ -111,18 +131,22 @@ namespace LearnHub_Api.Services
             if (user_Id is null)
                 return Result.Failure<AuthResponse>(UserErrors.InvalidJwtToken);
 
-            var user = await _UserManager.FindByIdAsync(user_Id);
+            var user = await _context.Users
+                .Include(x => x.RefreshTokens.Where(t => t.RevokedOn == null && t.ExpireOn > DateTime.UtcNow))
+                .FirstOrDefaultAsync(x => x.Id == user_Id, cancellationToken);
+
             if (user is null)
                 return Result.Failure<AuthResponse>(UserErrors.InvalidCredentials);
 
-
             var userRefreshToken = user.RefreshTokens
-                .SingleOrDefault(x => x.Token == RefreshToken && x.IsActive);
+                .SingleOrDefault(x => x.Token == RefreshToken);
             if (userRefreshToken is null)
                 return Result.Failure<AuthResponse>(UserErrors.InvalidRefreshToken);
 
             userRefreshToken.RevokedOn = DateTime.UtcNow;
-            await _UserManager.UpdateAsync(user);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
             return Result.Success();
         }
 
